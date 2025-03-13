08517505e4d08400fbbe3750a1df1dc89ed5542f
Iman Iqor <iqorimane111@gmail.com.com>
2025-03-13 21:55:35 +0000

    test

--- exec1.c
+++ exec1.c
@@ -1,75 +1,93 @@
-#include"header.h"
+#include "header.h"
 
-void init_game(t_game *game,char** two_d)
+void	init_game(t_game *game, char **two_d)
 {
-    int rows;
-    int columns;
-    rows = map_lines_count(two_d);
-    columns = ft_strlen(two_d[0]);
-    game->mlx = mlx_init();
-    if (!game->mlx)
-    {
-        write(2,"failed to init mlx\n",19);
-        exit(1);
-    }
+	int	rows;
+	int	columns;
 
-    game->win = mlx_new_window(game->mlx, columns*67, rows*67, "so_long");
-    if (!game->win)
-    {
-        write(2,"failed to create window\n",24);
-        mlx_destroy_display(game->mlx);
-
-        free(game->mlx);
-        exit(1);
-    }
+	rows = map_lines_count(two_d);
+	columns = ft_strlen(two_d[0]);
+	game->mlx = mlx_init();
+	if (!game->mlx)
+	{
+		write(2, "failed to init mlx\n", 19);
+		exit(1);
+	}
+	game->win = mlx_new_window(game->mlx, columns * 67, rows * 67, "so_long");
+	if (!game->win)
+	{
+		write(2, "failed to create window\n", 24);
+		mlx_destroy_display(game->mlx);
+		free(game->mlx);
+		exit(1);
+	}
 }
-void load_image(t_imane *imane)
+void	load_image(t_imane *imane)
 {
-    t_game* game = imane->game;
-    game->player_img = mlx_xpm_file_to_image(game->mlx, "pics/player.xpm", &game->img_width, &game->img_height);
-    game->coin_img = mlx_xpm_file_to_image(game->mlx, "pics/coin1.xpm", &game->img_width, &game->img_height);
-    game->wall_img = mlx_xpm_file_to_image(game->mlx, "pics/wall1.xpm", &game->img_width, &game->img_height);
-    game->exit_img = mlx_xpm_file_to_image(game->mlx, "pics/door1.xpm", &game->img_width, &game->img_height);
-    game->background_img = mlx_xpm_file_to_image(game->mlx, "pics/background1.xpm", &game->img_width, &game->img_height);
+	t_game	*game;
 
-    if (!game->player_img || !game->coin_img|| !game->wall_img || !game->exit_img || !game->background_img)
-    {
-        write(2,"could not load one or more images\n",34);
-        free(imane->game->mlx);
+	game = imane->game;
+	game->player_img = mlx_xpm_file_to_image(game->mlx, "pics/player1.xpm",
+			&game->img_width, &game->img_height);
+	game->coin_img = mlx_xpm_file_to_image(game->mlx, "pics/coin1.xpm",
+			&game->img_width, &game->img_height);
+	game->wall_img = mlx_xpm_file_to_image(game->mlx, "pics/wall1.xpm",
+			&game->img_width, &game->img_height);
+	game->exit_img = mlx_xpm_file_to_image(game->mlx, "pics/door1.xpm",
+			&game->img_width, &game->img_height);
+	game->background_img = mlx_xpm_file_to_image(game->mlx,
+			"pics/background1.xpm", &game->img_width, &game->img_height);
+	if (!game->player_img || !game->coin_img || !game->wall_img
+		|| !game->exit_img || !game->background_img)
+	{
+		write(2, "could not load one or more images\n", 34);
+		if (game->coin_img)
+			mlx_destroy_image(game->mlx, game->coin_img);
+		if (game->player_img)
+			mlx_destroy_image(game->mlx, game->player_img);
+		if (game->wall_img)
+			mlx_destroy_image(game->mlx, game->wall_img);
+		if (game->exit_img)
+			mlx_destroy_image(game->mlx, game->exit_img);
+		if (game->background_img)
+			mlx_destroy_image(game->mlx, game->background_img);
+		mlx_destroy_window(game->mlx, game->win);
+		mlx_destroy_display(imane->game->mlx);
+		free(imane->game->mlx);
 		free(imane->map->lines);
-        ftt_free(imane->map->map_two_d2);
-        ftt_free(imane->map->map_two_d);
-        exit(1);
-    }
+		ftt_free(imane->map->map_two_d2);
+		ftt_free(imane->map->map_two_d);
+		exit(1);
+	}
 }
 
-void render_image(t_game *game,char** two_d)
+void	render_image(t_game *game, char **two_d)
 {
-    int x, y;
-    y = 0;
-    game->var = NULL;
-    while (two_d[y])
-    {
-        x = 0;
-        while (two_d[y][x])
-        {
-            if (two_d[y][x] == 'P')
-                game->var= game->player_img;
-            else if (two_d[y][x] == 'C')
-                game->var= game->coin_img;
-            else if (two_d[y][x] == '0')
-                game->var= game->background_img;
-            else if (two_d[y][x] == '1')
-                game->var= game->wall_img;
-            else if (two_d[y][x] == 'E')
-                game->var= game->exit_img;
-            if (game->var)
-                mlx_put_image_to_window(game->mlx, game->win,game->var, x * 67, y * 67);
-
-            x++;
-        }
-        y++;
-    }
+	int x, y;
+	y = 0;
+	game->var = NULL;
+	while (two_d[y])
+	{
+		x = 0;
+		while (two_d[y][x])
+		{
+			if (two_d[y][x] == 'P')
+				game->var = game->player_img;
+			else if (two_d[y][x] == 'C')
+				game->var = game->coin_img;
+			else if (two_d[y][x] == '0')
+				game->var = game->background_img;
+			else if (two_d[y][x] == '1')
+				game->var = game->wall_img;
+			else if (two_d[y][x] == 'E')
+				game->var = game->exit_img;
+			if (game->var)
+				mlx_put_image_to_window(game->mlx, game->win, game->var, x * 67,
+					y * 67);
+			x++;
+		}
+		y++;
+	}
 }
 
 int	key_handler(int keycode, t_imane *imane)
@@ -91,7 +109,7 @@ int	key_handler(int keycode, t_imane *imane)
 
 int	close_window(void *ptr)
 {
-	t_imane	*imane;
+	t_imane *imane;
 
 	imane = (t_imane *)ptr;
 	ftt_free(imane->map->map_two_d);
